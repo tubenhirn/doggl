771ddc0ce7d939f59d1c2f84c7dcde7295f21acd
Johannes Stang <tubenhirn@gmail.com>
2023-03-02 10:13:45 +0100

    ci: add flag for dagger go sdk release

--- ci/ci.go
+++ ci/ci.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 
 	"github.com/tubenhirn/dagger-ci-modules/v2/goreleaser"
 )
 
 func main() {
+	snapshot := flag.Bool("snapshot", true, "the string of the platform to run renovate on.")
+	flag.Parse()
+
+	fmt.Println("snapshot build " + strconv.FormatBool(*snapshot))
+
+
 	dir, _ := os.Getwd()
 
 	version, err := ioutil.ReadFile(dir + "/version")
@@ -19,11 +27,12 @@ func main() {
 
 	options := goreleaser.GoReleaserOpts{
 		Source:     dir,
-		Snapshot:   true,
+		Snapshot:   *snapshot,
 		RemoveDist: true,
 		Env: map[string]string{
 			"APP_VERSION": string(version),
 		},
+		Secret: []string{"GITHUB_TOKEN"},
 	}
 
 	if err := goreleaser.Release(context.Background(), options); err != nil {
