3d182665278cc167d062faaa52a61c548c09846b
Johannes Stang <tubenhirn@gmail.com>
2023-03-04 15:55:50 +0100

    ci: update ci pipeline

--- ci/ci.go
+++ ci/ci.go
@@ -7,15 +7,33 @@ import (
 	"io/ioutil"
 	"os"
 
+	"dagger.io/dagger"
 	"github.com/tubenhirn/dagger-ci-modules/v2/goreleaser"
 )
 
 func main() {
+	ctx := context.Background()
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		panic(err)
+	}
+
+	defer client.Close()
+
 	snapshot := flag.Bool("snapshot", true, "the string of the platform to run renovate on.")
 	clean := flag.Bool("clean", true, "the string of the platform to run renovate on.")
 	flag.Parse()
 
 	fmt.Println("running with flags:", "\nsnapshot", *snapshot, "\nremovedist", *clean)
+
+	var secrets = make(map[string]dagger.SecretID)
+	githubTokenId, err := client.Host().EnvVariable("GITHUB_TOKEN").Secret().ID(ctx)
+	if err != nil {
+		panic(err)
+	}
+	secrets["GITHUB_TOKEN"] = githubTokenId
 
 	dir, _ := os.Getwd()
 
@@ -31,10 +49,10 @@ func main() {
 		Env: map[string]string{
 			"APP_VERSION": string(version),
 		},
-		Secret: []string{"GITHUB_TOKEN"},
+		Secret: secrets,
 	}
 
-	if err := goreleaser.Release(context.Background(), options); err != nil {
+	if err := goreleaser.Release(context.Background(), *client, options); err != nil {
 		fmt.Println(err)
 	}
 }
