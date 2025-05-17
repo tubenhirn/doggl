ab7ef4179d68d5f207016820e43f0d3851be2d78
Johannes Stang <tubenhirn@gmail.com>
2025-05-18 01:51:57 +0200

    feat: rebuild with nix (#110)

--- release.config.js
+++ release.config.js
@@ -1,3 +1,44 @@
+const commitAnalyzerOptions = {};
+const releaseNotesGeneratorOptions = {};
+
 module.exports = {
-  extends: "/opt/base.config.js",
+  branches: ["main"],
+  plugins: [
+    ["@semantic-release/commit-analyzer", commitAnalyzerOptions],
+    ["@semantic-release/release-notes-generator", releaseNotesGeneratorOptions],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "semantic-release-replace-plugin",
+      {
+        replacements: [
+          {
+            files: ["version"],
+            from: "v.*",
+            to: "v${nextRelease.version}",
+            results: [
+              {
+                file: "version",
+                hasChanged: true,
+                numMatches: 1,
+                numReplacements: 1,
+              },
+            ],
+            countMatches: true,
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["version", "CHANGELOG.md"],
+      },
+    ],
+    ["@semantic-release/github"],
+  ],
 };
